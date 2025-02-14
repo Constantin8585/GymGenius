29cb2a57d5abbea65c803eb06cbea00ce6d0f59c
Constantin8585 <gadegbeabednego@gmail.com>
2025-02-14 03:40:24 +0000

    Ajout de la gestion du rôle utilisateur pour afficher ou masquer les sections d'accès rapide dans le tableau de bord, amélioration de l'interaction avec un effet de transition sur le bouton.

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StatsService } from '../services/Statistiques/stats.service';
-
+import { AuthService } from '../services/auth.service';  // Importer le service d'authentification
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  imports: [CommonModule],
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
   stats: any = {};
+  userRole: string | null = '';  // Propriété pour stocker le rôle de l'utilisateur
 
-  constructor(private statsService: StatsService, private router: Router) {}
+  constructor(private statsService: StatsService, private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
     this.loadStats();
+    this.userRole = this.authService.getUserRole();  // Récupérer le rôle de l'utilisateur
   }
 
   loadStats() {
